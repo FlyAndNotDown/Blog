73210f154c6efb3b7b1e82ece5d37f425ecb8083
John Kindem <461425614@qq.com>
2018-01-22 19:30:43 +0800

    完成主页部分，等待调试

--- main/models.py
+++ main/models.py
@@ -11,14 +11,24 @@ class Tag(models.Model):
     name = models.CharField(max_length=100)
 
 
+class Image(models.Model):
+    """
+    图片 Model
+    1. location char(200) 图片位置
+    """
+    # 图片位置
+    location = models.CharField(max_length=200)
+
+
 class Post(models.Model):
     """
-    文章
+    文章 Model
     # 1. title char(70) 标题
     # 2. body text 正文
     # 3. created_time datetime 创建时间
     # 4. modified_time datetime 最后修改时间
     # 5. tags many2many 标签
+    # 6. images many2many 图片
     """
     # 标题
     title = models.CharField(max_length=70)
@@ -30,3 +40,5 @@ class Post(models.Model):
     modified_time = models.DateTimeField()
     # 标签
     tags = models.ManyToManyField(Tag, blank=True)
+    # 图片
+    images = models.ManyToManyField(Image, blank=True)
--- main/views.py
+++ main/views.py
@@ -1,12 +1,44 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.core.paginator import Paginator
 from .models import Post, Tag
+
+
+# 首页每一页的文章数量
+INDEX_POST_PER_PAGE = 4
 
 
 # Create your views here.
 def index(request):
     # 获取所有文章，按照时间顺序排序
     posts = Post.objects.all().order_by('-created_time')
+    # 获取现在是第几页
+    page = 1
+    if request.GET.get('page') is not None:
+        page = request.GET.get('page')
+    # 错误检测
+    if type(page) != int:
+        return Http404()
+    # 获取总页数
+    page_num = len(posts) / INDEX_POST_PER_PAGE + 1
+    # 错误检测
+    if not 0 < page <= page_num:
+        return Http404()
+    # 分页器
+    paginator = Paginator(posts, INDEX_POST_PER_PAGE)
+    posts_this_page = paginator.page(page)
+    # 判断是否为第一页或最后一页
+    is_first_page = page == 1
+    is_last_page = page == page_num
+
+    # 渲染
     return render(request, 'main/index.html', context={
-        'posts': posts
+        'title': '首页-Kindem的博客',
+        'is_first_page': is_first_page,
+        'is_last_page': is_last_page,
+        'posts1': posts_this_page[0:2],
+        'posts2': posts_this_page[2:],
+        'page': page,
+        'pre_page': page - 1,
+        'next_page': page + 1
     })
