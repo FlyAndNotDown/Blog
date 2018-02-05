f04ab41aa9d131f92896d6f16ba5f700c6c74fe0
John Kindem <461425614@qq.com>
2018-02-06 00:25:47 +0800

    添加360自动提交功能

---
+++ main/static/main/lib/360/auto-submit.js
@@ -0,0 +1,4 @@
+(function(){
+var src = (document.location.protocol == "http:") ? "http://js.passport.qihucdn.com/11.0.1.js?9a8f763a16fdab8e1baa96f39826ccf4":"https://jspassport.ssl.qhimg.com/11.0.1.js?9a8f763a16fdab8e1baa96f39826ccf4";
+document.write('<script src="' + src + '" id="sozz"><\/script>');
+})();
