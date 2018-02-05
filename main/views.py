54f8dc801eff87f89268071f3baa016c910f0941
John Kindem <461425614@qq.com>
2018-02-05 20:33:52 +0800

    debug

--- main/views.py
+++ main/views.py
@@ -388,6 +388,7 @@ def login_qq(request):
         k_user.save()
 
         # 返回，并重定向到登录前的网站
+        return HttpResponse(request.session['login_from'])
         return HttpResponseRedirect(request.session['login_from'])
 
 
