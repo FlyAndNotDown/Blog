ed51ae4ab1efb92cf73a312797cc2b41b46ccf82
John Kindem <461425614@qq.com>
2018-02-06 22:58:52 +0800

    bug fix

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Tag, KUser
+from .models import Post, Tag, KUser, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -12,7 +12,11 @@ class TagAdmin(admin.ModelAdmin):
 class KUserAdmin(admin.ModelAdmin):
     list_display = ['user_type', 'nickname', 'uid', 'avatar']
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['sender', 'receiver', 'post', 'level', 'parent', 'time', 'context']
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag, TagAdmin)
 admin.site.register(KUser, KUserAdmin)
+admin.site.register(Comment, CommentAdmin)
